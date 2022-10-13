9b51c1ed51e91e951c8038b3f9f47ea1903fcd5d
Angie <angie.monnye@gmail.com>
2022-10-13 09:37:09 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
   *main - this is the entry point of the program
   *
   *puts - will be used to display the output
-  *retun - this will return 0 when the condition is met
+  *return - this will return 0 when the condition is met
   */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
