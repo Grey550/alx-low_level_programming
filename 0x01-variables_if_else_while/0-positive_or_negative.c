015ffc69b734ac27ddf684993e7e8328f5534a24
Angie <angie.monnye@gmail.com>
2022-10-14 15:47:09 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,16 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
-  *we have called the stdlib for using libraries
-  *we have called the time header is used to provide access to time
-  *and date manipulation and formatting
-  */
 
 /**
-  * main - Entry point
-  *
-  * Description: the program generates random numbers
+  * main - Determines if the number is positive, zero or negative
   *
   * Return: Always 0
   */
@@ -25,15 +18,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive %d\n", rand());
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero %d\n", rand());
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative %d\n", rand());
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
