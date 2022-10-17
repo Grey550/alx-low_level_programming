8b58fefbe8dd62659dac87e90b109ed0b9edccf4
Angie <angie.monnye@gmail.com>
2022-10-17 14:00:04 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers using ASCII
+ *
+ * Return : Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
