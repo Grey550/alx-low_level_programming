afb17a0ac6c15c3628252adb010ebb2172b211cd
Angie <angie.monnye@gmail.com>
2022-10-19 13:29:02 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,4 +7,21 @@
  */
 
 void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 28; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
 
