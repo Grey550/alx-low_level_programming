082856039f10e1703a96923e0dabb2655a0093a6
Angie <angie.monnye@gmail.com>
2022-10-23 10:41:37 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,25 @@
 
 int main(void)
 {
-	int x;
+	int y;
 
-	for (x = 1; x <= 100; x++)
+	for (y = 1; y <= 100; y++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if ((y % 3 == 0) && (y % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if (x % 3 == 0)
+		else if (y % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (x % 5 == 0)
+		else if (y % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d ", y);
 		}
 	}
 	printf("\n");
