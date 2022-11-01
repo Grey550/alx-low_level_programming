de1226b5a12399c4019915b92d6b6dc2bacacbd5
Angie <angie.monnye@gmail.com>
2022-11-01 03:35:29 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings using n bytes
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: third string
+ * Return: a pointer resulting to string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	_strncat(dest, src, n);
+	return (dest);
+
+}
