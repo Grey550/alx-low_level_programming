501cd54f60aae7571f63e26b00c5941b20dd7e22
Angie <angie.monnye@gmail.com>
2022-11-01 05:57:08 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies one string to the other
+ *
+ * @dest: first string
+ * @src: second string
+ * @n: integer value
+ *
+ * Return: copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(src, dest, n);
+	return (src);
+}
+
