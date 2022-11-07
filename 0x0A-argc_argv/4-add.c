ee77dec7fe1c2b161bb3b642ebcbf6c2de7f47ca
Angie <angie.monnye@gmail.com>
2022-11-06 16:16:24 -0800

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, results = 0;
+
+	for (x = 1; x < argc; argc++)
+	{
+		for (y = 0; argv[x][y]; y++)
+		{
+			if (argv[x][y] < 48 || argv[x][y] > 57)
+			{
+				printf("Errot\n");
+				return (1);
+			}
+		}
+		results += atoi(argv[x]);
+	}
+	printf("%d\n", results);
+	return (0);
+}
