fd43a8f189e23cd365bcc64fb6a24a3c164ce8db
Angie <angie.monnye@gmail.com>
2023-04-10 04:11:07 +0200

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - newly created hash table
+ *
  * @size: the size of the array
  * Return: returns a pointer to the newly created hash table
  */
@@ -10,11 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		return (NULL);
+	}
 	return (hash_table);
 }
